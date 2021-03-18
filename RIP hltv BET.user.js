7ab81188885a892236565804e1404deab3938b20
Frank <472730949@qq.com>
2021-03-18 17:53:38 +0800

    Update RIP hltv BET.user.js

--- RIP hltv BET.user.js
+++ RIP hltv BET.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
-// @name         RIP hltv BET
-// @namespace    https://github.com/wolfcon/RIP-HLTV-BET
-// @version      0.1
-// @description  Remove hltv.org Annoy AD
-// @author       Frank
-// @match        https://www.hltv.org/*
-// @icon         https://www.hltv.org/img/static/favicon/favicon-32x32.png
-// @run-at       document-end
+// @name                RIP HLTV BET
+// @name:zh-CN          HLTV 广告去除插件
+// @namespace           https://github.com/wolfcon/RIP-HLTV-BET
+// @version             0.1
+// @description         Remove hltv.org Annoy AD
+// @description:zh-cn   清除那些🤮背景赌博广告.
+// @author              Frank
+// @match               https://www.hltv.org/*
+// @icon                https://www.hltv.org/img/static/favicon/favicon-32x32.png
+// @run-at              document-end
 // ==/UserScript==
 
 (function() {
@@ -15,4 +17,3 @@
     document.body.removeAttribute("data-href");
     document.body.removeAttribute("style");
 })();
-
