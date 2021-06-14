e1662a35ff0d6e8b505e9ba208b456282b3152c0
Frank <472730949@qq.com>
2021-06-14 18:31:36 +0800

    Remove other advertisements in some tricky way, but not perfect.

--- RIP hltv BET.user.js
+++ RIP hltv BET.user.js
@@ -2,19 +2,56 @@
 // @name                RIP HLTV BET
 // @name:zh-CN          HLTV 广告去除插件
 // @namespace           https://github.com/wolfcon/RIP-HLTV-BET
-// @version             0.1
+// @version             0.2
 // @description         Remove hltv.org Annoy AD
 // @description:zh-cn   清除那些🤮背景赌博广告.
 // @author              Frank
+// @require             https://code.jquery.com/jquery-3.6.0.min.js
 // @match               https://www.hltv.org/*
 // @icon                https://www.hltv.org/img/static/favicon/favicon-32x32.png
 // @license             MIT
 // @run-at              document-end
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    document.body.removeAttribute("data-href");
-    document.body.removeAttribute("style");
+    function removeNonOfficialSiteElement(element) {
+        $.each(element.children(), function (index, ele) {
+            const adA = $(ele).find('a').not('[href^="/"]');
+            $(adA).remove();
+            // const finded = adA.length;
+            // if (finded > 0) {
+            //     $(ele).remove();
+            // }
+        });
+    }
+
+    (function removeBackgroundAd() {
+        document.body.removeAttribute("data-href");
+        document.body.removeAttribute("style");
+    })();
+
+    (function removeTopAds() {
+        var topDiv = document.getElementsByClassName("logoCon")[0];
+        var adCount = topDiv.children.length - 1;
+
+        while (adCount > 0) {
+            topDiv.removeChild(topDiv.lastElementChild);
+            adCount--;
+        }
+    })();
+
+    (function removeLeftColumnAd() {
+        removeNonOfficialSiteElement($('.leftCol'));
+    })();
+
+    (function removeRightColumnAd() {
+        removeNonOfficialSiteElement($('.rightCol'));
+        removeNonOfficialSiteElement($('.right2Col'))
+    })();
+
+    (function removeContentColumnAd() {
+        removeNonOfficialSiteElement($('.contentCol'));
+    })();
 })();
