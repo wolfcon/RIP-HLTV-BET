390b05199aa84f498a77d963f067c349cff1d2b3
Frank <472730949@qq.com>
2021-09-30 17:21:54 +0800

    Change the time that script runs at to avoid ad flashing problem.

--- RIP-hltv-BET.user.js
+++ RIP-hltv-BET.user.js
@@ -2,7 +2,7 @@
 // @name                RIP HLTV BET
 // @name:zh-CN          HLTV 广告去除插件
 // @namespace           https://github.com/wolfcon/RIP-HLTV-BET
-// @version             0.4
+// @version             1.0
 // @description         Remove hltv.org Annoy AD
 // @description:zh-cn   清除那些🤮背景赌博广告.
 // @author              Frank
@@ -10,7 +10,7 @@
 // @match               https://www.hltv.org/*
 // @icon                https://www.hltv.org/img/static/favicon/favicon-32x32.png
 // @license             MIT
-// @run-at              document-end
+// @run-at              document-body
 // ==/UserScript==
 
 const filters = [
