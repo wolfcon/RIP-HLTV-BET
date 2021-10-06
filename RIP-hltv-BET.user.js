14faf91f0f2e04d41c7bd12be49758160e71b4b2
Frank <472730949@qq.com>
2021-10-06 10:44:31 +0800

    Removed some redundant code

--- RIP-hltv-BET.user.js
+++ RIP-hltv-BET.user.js
@@ -2,7 +2,7 @@
 // @name                RIP HLTV BET
 // @name:zh-CN          HLTV 广告去除插件
 // @namespace           https://github.com/wolfcon/RIP-HLTV-BET
-// @version             1.2
+// @version             1.3
 // @description         Remove hltv.org Annoy AD
 // @description:zh-cn   清除那些🤮背景赌博广告.
 // @author              Frank
@@ -13,53 +13,24 @@
 // @run-at              document-body
 // ==/UserScript==
 
-const filters = '[class*="yabo"], [href*="bet"], a:not([href^="/"])';
+const filters = '[class*="yabo"], [href*="bet"], [class*="regional"], [class*="world"], a:not([href^="/"])';
 // Use ADBlock way to block some annoy element
-function removeFilters() {
+(function removeFilters() {
     var $hiddenStyle = $('<style type="text/css"></style>');
     $($('head')[0]).append($hiddenStyle);
     $hiddenStyle.append(filters + "{display: none !important; visibility: hidden !important;}");
-}
+})();
 
-function removeAll() {
-    'use strict';
+// removeBackgroundAd
+document.body.removeAttribute("data-href");
+document.body.removeAttribute("style");
 
-    function removeNonOfficialSiteElement(element) {
-        const adA = $(element).find('a').not('[href^="/"]');
-        $(adA).remove();
+(function removeTopAds() {
+    var topDiv = document.getElementsByClassName("logoCon")[0];
+    var adCount = topDiv.children.length - 1;
+
+    while (adCount > 0) {
+        topDiv.removeChild(topDiv.lastElementChild);
+        adCount--;
     }
-
-    function removeClassElement(filter) {
-        const ele = $('div,aside').find(filter);
-        $(ele).remove();
-    }
-
-    // removeBackgroundAd
-    document.body.removeAttribute("data-href");
-    document.body.removeAttribute("style");
-
-    (function removeTopAds() {
-        var topDiv = document.getElementsByClassName("logoCon")[0];
-        var adCount = topDiv.children.length - 1;
-
-        while (adCount > 0) {
-            topDiv.removeChild(topDiv.lastElementChild);
-            adCount--;
-        }
-    })();
-
-    // removeLeftColumnAd
-    removeNonOfficialSiteElement($('.leftCol'));
-
-    // removeRightColumnAd
-    removeNonOfficialSiteElement($('.rightCol'));
-    removeNonOfficialSiteElement($('.right2Col'));
-
-    // removeContentColumnAd
-    removeNonOfficialSiteElement($('.contentCol'));
-
-    // remove extra filters
-    removeFilters();
-};
-
-removeAll();
+})();
