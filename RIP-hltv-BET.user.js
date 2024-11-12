e29c72b9218e85ef17934202aa4263a108a54141
Frank <472730949@qq.com>
2024-11-12 18:12:47 +0800

    Bump version to 2.0

--- RIP-hltv-BET.user.js
+++ RIP-hltv-BET.user.js
@@ -2,7 +2,7 @@
 // @name                RIP HLTV BET
 // @name:zh-CN          HLTV 广告去除插件
 // @namespace           https://github.com/wolfcon/RIP-HLTV-BET
-// @version             1.13
+// @version             2.0
 // @description         Remove hltv.org Annoy AD
 // @description:zh-cn   清除那些🤮背景赌博广告.
 // @author              Frank
